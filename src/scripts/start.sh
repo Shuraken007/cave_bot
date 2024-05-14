72c94d0becf1af5334628e9266641d601fc42e89
Shuraken007 <alexander.mad93@gmail.com>
2024-05-15 01:44:45 +0300

    cosmetic changes

--- src/scripts/start.sh
+++ src/scripts/start.sh
@@ -2,5 +2,7 @@ curl -sSL https://install.python-poetry.org | python
 ~/.local/bin/poetry install --no-root
 cd ~
 ~/.local/bin/poetry run alembic upgrade head
-echo 'alembic upgraded'
+echo 'upgraded'
+~/.local/bin/poetry run pytest -n auto
+echo 'tests finished'
 ~/.local/bin/poetry run python -m src.scripts.start
