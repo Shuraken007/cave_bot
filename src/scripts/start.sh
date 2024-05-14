fd6577bc56add1534b39f09ce1c6683e22998f7d
Shuraken007 <alexander.mad93@gmail.com>
2024-05-15 01:17:25 +0300

    fixed poetry path

--- src/scripts/start.sh
+++ src/scripts/start.sh
@@ -1,7 +1,6 @@
 curl -sSL https://install.python-poetry.org | python
-p=~/.local/bin/poetry
-$p install --no-root
+~/.local/bin/poetry install --no-root
 cd ~
-$p run alembic upgrade head
-$p run pytest -n auto
-$p run python -m src.scripts.start
+~/.local/bin/poetry run alembic upgrade head
+~/.local/bin/poetry run pytest -n auto
+~/.local/bin/poetry run python -m src.scripts.start
