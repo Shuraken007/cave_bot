83b71663b4b0ae4eb2cd716681923c8b4ad46236
Shuraken007 <alexander.mad93@gmail.com>
2024-05-17 16:43:08 +0300

    fixed alter_column for mysql at migration script

--- cave_bot/scripts/start.sh
+++ cave_bot/scripts/start.sh
@@ -1,8 +1,8 @@
 curl -sSL https://install.python-poetry.org | python
-~/.local/bin/poetry install --no-root
+~/.local/bin/poetry install
 cd ~
 ~/.local/bin/poetry run alembic upgrade head
 echo 'upgraded'
-~/.local/bin/poetry run pytest
-echo 'tests finished'
+# ~/.local/bin/poetry run pytest
+# echo 'tests finished'
 ~/.local/bin/poetry run start
